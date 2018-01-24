c2a76c7a5ec41d658c2d4849affc1c7ba34f79b1
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-01-24 15:51:15 -0300

    comentários da callback apagados

--- codigo_de_leitura/codigo_de_leitura.ino
+++ codigo_de_leitura/codigo_de_leitura.ino
@@ -57,17 +57,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
   Serial.println("--------------------");
 
-  /*
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-  */
-
+  
 }
 
 void reconnect() {
