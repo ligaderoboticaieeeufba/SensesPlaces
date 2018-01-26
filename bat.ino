6c4fc2000252c485c797036debf5c10609503472
RodAgm <agrmcd@gmail.com>
2018-01-26 15:56:00 -0300

    Update bat.ino

--- bat.ino
+++ bat.ino
@@ -1,32 +1,38 @@
+int  previous_running_time=0;
+int current_running_time=0;
+
+
+int  analog_0 = 0;
+int counter = 0;
+int  target = 10000;
+int  pulse = 0 ;
+int  remind = 0;
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(11500);
+  Serial.begin(9600);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-int  analog_0 = 0;
-int counter = 0;
-int counter2=0;
-int  previous_running_time = 0 ;
-int  target = 10000;
-int  pulse = 0 ;
-int  remind = 0;
-int current_running_time= 0;
+     
+     
 
-  while (true){
-    
-      current_running_time = millis();
-      analog_0 = analogRead(0);
-
+ analog_0 = analogRead(0);    
+  current_running_time = millis()  ;
 
       if (remind == 0 ){
 
           if ( analog_0 >= 920){
             
            counter = counter+1;
+          
            remind = 1;
      
             }
@@ -40,29 +46,30 @@ int current_running_time= 0;
      
             }   
         }
-
         
       }
     
 
-    if( ( current_running_time - previous_running_time) >= target){
+    if( current_running_time - previous_running_time >= target){
       
-      counter = counter *6;
+      counter = counter *6; 
+   
 
-      Serial.println(counter);
-      
-      
-      
-      
-      
+      previous_running_time = millis();
+
+      Serial.print ("counter");
+     Serial.println(counter);
+       counter = 0 ;
       }
-    
 
+       
+       Serial.print( "previous");
+      Serial.println(previous_running_time);
 
-    
-    
-    
-    }
+       Serial.print("current");
+      Serial.println(current_running_time);
   
+    
+    }   
 
-}
+    
