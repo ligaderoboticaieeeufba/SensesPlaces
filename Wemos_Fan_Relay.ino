73b4c8958420e3abdbb8f2f3dc7b6330482104fb
Andre Bahiense <30840318+andrebahiense@users.noreply.github.com>
2018-01-25 22:31:12 -0300

    Update Wemos_Fan_Relay.ino

--- Wemos_Fan_Relay.ino
+++ Wemos_Fan_Relay.ino
@@ -1,4 +1,5 @@
-/* Código para ligação do Wemos no Relé e ativação do ventilador
+/* Código para ligação do Wemos no Relé e ativação do ventilador.
+O sensor a ser utilizado para controlar o ventilador será um Termistor
 
 
 Usar a saída 'r' por questão de pinagem diferente do Wemos D1 mini para o Arduino. 
@@ -14,6 +15,8 @@ void setup (){
 void loop(){
   int valoranalogico; // valor a ser recebido pela transmissão de dados
 
+// Os valores de XX, YY e ZZ devem ser colocados após verificação experimental
+
 	if (valoranalogico >= XX ){ // Para temperatura alta
 	  digitalWrite(r, HIGH);
     delay(10000);
@@ -21,13 +24,13 @@ void loop(){
     delay(2000);
 	}
 
-  else if(valoranalogico <= XX && valor analogico >= YY){
+  else if(valoranalogico <= XX && valor analogico >= YY){ // Para Temperatura mediana
     digitalWrite(r, HIGH);
     delay(3000);
     digitalWrite(r, LOW);
     delay(2000);
   }
-  else (valoranalogico <= ZZ){
+  else (valoranalogico <= ZZ){ // Para temperatura baixa
     digitalWrite(r, HIGH);
     delay(1000);
     digitalWrite(r, LOW);
